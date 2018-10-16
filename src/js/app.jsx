a60ce4b8d41cf239b58c8e6c3e44490edd1c7fd9
devwiltbank <devin.wiltbank@gmail.com>
2018-10-15 19:02:11 -0700

    Testing added to the mortgage calculator

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,9 +1,54 @@
 import React, { Component } from 'react';
+const Mortgage = require('../../src/lib/Mortgage');
 
-export default class App extends Component {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      principal: 0,
+      interestRate: 0,
+      loanTerm: 0,
+      period: 12,
+      monthlyPayment: 0
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.calculateMonthlyPayment = this.calculateMonthlyPayment.bind(this)
+  }
+
+  handleChange(event) {
+    switch (event.target.name) {
+      case 'principal':
+        this.setState({ principal: Number(event.target.value) })
+        break;
+      case 'loanTerm':
+        this.setState({ loanTerm: Number(event.target.value) })
+        break;
+      case 'period':
+        this.setState({ period: Number(event.target.value) })
+        break;
+      case 'interestRate':
+        this.setState({ interestRate: Number(event.target.value) })
+        break;
+      default:
+        break;
+    }
+  }
+
+  calculateMonthlyPayment() {
+    let mortgage = new Mortgage(
+      this.state.principal,
+      this.state.interestRate,
+      this.state.loanTerm,
+      this.state.period)
+    let monthlyPayment = mortgage.monthlyPayment()
+    let monthly = document.getElementById('output')
+    monthly.innerText = "$" + monthlyPayment
+  }
+  
   render() {
     return (
       <div className='App'>
+        <h1>Mortgage Calculator</h1>
         It works!
       </div>
     );
--- test/unit/mortgage.spec.js
+++ test/unit/mortgage.spec.js
@@ -1,6 +1,29 @@
 const expect = require('chai').expect;
 const Mortgage = require('../../src/js/lib/Mortgage');
 
-describe('Mortgage Calculator', () => {
+describe('Mortgage', () => {
+    let mortgage = null;
+     
+    beforeEach(() => {
+      mortgage = new Mortgage();
+    });
+
+    it('it should have a constructor', () => {
+        expect(mortgage.constructor).to.exist;
+    });
+    
+    it('it should have a function named monthlyPayment', () => {
+        expect(mortgage.monthlyPayment).to.exist;
+    });
+
+    it('should have a function which returns a number', () => {
+        expect(mortgage.monthlyPayment()).to.be.a('number');
+    });
+
+    it('monthlyPayment function should calculate correctly', () => {
+        mortgageTest = new Mortgage (100000, 3.75, 15, 12);
+        expect(mortgageTest.monthlyPayment()).to.equal(727.22);
+    });
+    
 
 });
